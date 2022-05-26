14dc305434ab072be0a80f189f2fd9306584ed90
Vlad <vladkorotki92@gmail.com>
2022-05-27 00:23:40 +0300

    fix: change structure project

---
+++ front-end/js/dataLayer/usersDataLayer.js
@@ -0,0 +1,40 @@
+import { DataLayer } from "./dataLayer.js";
+
+export class UsersDataLayer extends DataLayer {
+	constructor(options) {
+		super(options);
+		this.dataTableName = options.dataTableName;
+	}
+
+	addTable() {
+		super.addTable(this.dataTableName);
+	}
+
+	add(userObject, keyProperty) {
+		return super.add(this.dataTableName, userObject, keyProperty);
+	}
+
+
+
+
+	delete(userObject) {
+		return super.delete(this.dataTableName, userObject);
+	}
+
+	compareUsers(userMail, userPassword) {
+		return super.compareUsers(this.dataTableName, userMail, userPassword);
+	}
+
+	allUsers() {
+		return super.getUsers(this.dataTableName);
+	}
+
+	currentUser(key) {
+		return this.allUsers()[key];
+	}
+
+}
+
+export const usersDataLayer = new UsersDataLayer({
+	dataTableName: 'Users'
+});
---
+++ front-end/js/forms/formEnter.js
@@ -0,0 +1,67 @@
+import { Form } from './form.js';
+
+import { popup } from '../pageTools/popUp.js';
+import { usersDataLayer } from '../dataLayer/usersDataLayer.js';
+import { panelUser } from '../usersBuild/userPanel.js';
+
+
+export class FormSignIn extends Form {
+	constructor(options) {
+		super(options.id);
+		this.starterForm = document.querySelector(options.starterForm);
+		this.userPanel = panelUser;
+
+		// this.userPanel = new UserPanel({
+		// 	userPanel: '.user__panel',
+		// 	initialForm: '.initial__form',
+		// 	btnExit: '.btn--exit',
+		// 	btnEdit: '.btn--edit',
+		// 	btnBack: '.btn--back',
+		// 	btnHideClass: 'btn--hide',
+		// 	initialFormActiveClass: 'initial__form--active',
+		// 	userPanelActiveCLass: 'user__panel--active',
+		// 	eventType: 'click',
+		// });
+
+		if (typeof FormSignIn.instance === 'object') {
+			return FormSignIn.instance;
+		}
+		FormSignIn.instance = this;
+		return this;
+
+	}
+
+	signIn(event) {
+		const compare = usersDataLayer.compareUsers(this.userData()['e-mail'], this.userData().password);
+		if (compare) {
+			alert('hi');
+			this.userPanel.showPanel(this.userData()['e-mail'], event);
+			usersDataLayer.createToken(this.userData());
+			popup.close(event)
+		} else {
+			alert('Не верный e-mail или пароль')
+		}
+	}
+
+	onFormSubmit(event) {
+		const submited = super.onFormSubmit(event);
+		if (submited) {
+			usersDataLayer.addTable(this.dataTableName);
+			this.userData();
+			this.signIn(event);
+		}
+	}
+
+	userData() {
+		let user = {};
+		const inputs = this.inputs;
+		for (let i = 0; i < inputs.length; i++) {
+			user[inputs[i].name] = inputs[i].value;
+		}
+		return user;
+	}
+
+
+
+}
+
